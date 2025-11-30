a9436f84dffb29cfc59002a63953f23034363036
rakamsite <rakamsite@gmail.com>
2025-11-30 22:29:00 +0330

    Add navigation buttons for ticket pages

--- templates/ticket-list.php
+++ templates/ticket-list.php
@@ -20,6 +20,9 @@ $tickets = new WP_Query($args);
 ?>
 
 <div class="container mx-auto p-4 max-w-4xl dir-rtl">
+    <div class="flex justify-start mb-4">
+        <a href="https://dede.ir/ticket/" class="bg-[#2f2483] text-white py-3 rounded-md px-4">ثبت درخواست جدید</a>
+    </div>
     <?php if ($tickets->have_posts()): ?>
         <table class="w-full border-collapse bg-white shadow-md rounded-lg">
             <thead>
