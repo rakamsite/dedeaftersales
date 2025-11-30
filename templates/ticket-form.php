a4935e7d1747c33c720b8852af669d4b7b7ef802
rakamsite <rakamsite@gmail.com>
2025-11-30 20:09:50 +0330

    Force login button text to white

--- templates/ticket-form.php
+++ templates/ticket-form.php
@@ -1,16 +1,18 @@
 <?php
 if (!is_user_logged_in()) {
-    return <<<HTML
+    echo <<<HTML
 <div class="w-full flex justify-center items-center mt-20">
     <button data-drawer-hide="mobile-menu" class="bg-[#2F2483] w-fit rounded-lg h-fit text-white flex py-3 px-5 mt-1/2 flex justify-between login_register_page">
         <svg width="21" height="26" viewBox="0 0 21 26" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M10.3334 10.3333C13.1868 10.3333 15.5 8.02014 15.5 5.16667C15.5 2.3132 13.1868 0 10.3334 0C7.47988 0 5.16669 2.3132 5.16669 5.16667C5.16669 8.02014 7.47988 10.3333 10.3334 10.3333Z" fill="white"></path>
             <path d="M20.6667 20.0208C20.6667 23.2306 20.6667 25.8333 10.3333 25.8333C0 25.8333 0 23.2306 0 20.0208C0 16.8111 4.62675 14.2083 10.3333 14.2083C16.0399 14.2083 20.6667 16.8111 20.6667 20.0208Z" fill="white"></path>
         </svg>
-        <a class="mr-2">ورود به حساب کاربری </a>
+        <a class="mr-2 text-white" style="color: #fff !important;">ورود به حساب کاربری </a>
     </button>
 </div>
 HTML;
+
+    return;
 }
 ?>
 
